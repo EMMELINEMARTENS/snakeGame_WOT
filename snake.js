3839b095a6be127a4addefb4b0d918cfdce1833e
Emmeline Martens <50316143+EMMELINEMARTENS@users.noreply.github.com>
2020-10-07 22:23:10 +0200

    snake game

---
+++ snake.js
@@ -0,0 +1,128 @@
+const canvas = document.getElementById('snake');
+const ctx = canvas.getContext("2d");
+
+// create the unit (field)
+const box = 32;
+
+//load images
+const ground = new Image();
+ground.src = 'images/ground.png';
+
+const apple = new Image();
+apple.src = 'images/food.png';
+
+// load audio
+const dead = new Audio();
+const eat = new Audio();
+
+
+dead.src ="audio/dead.mp3";
+dead.src = "audio/eat.mp3";
+
+
+// create snake (array)
+let snake = [];
+snake[0]= {
+	x: 9 * box,
+	y: 10 * box
+}
+
+// create food (object)
+let food = {
+	x:Math.floor(Math.random()* 17+1) * box,
+	y:Math.floor(Math.random()* 15+3) * box
+}
+
+// create the score variable
+let score = 0;
+let d;
+
+// control the snake
+document.addEventListener('keydown', direction);
+
+function direction(event) {
+	if (event.keyCode == 37 && d != "RIGHT"){
+	d = "LEFT";
+}else if(event.keyCode == 38 && d != "DOWN"){
+	d = "UP";
+	} else if (event.keyCode == 39 && d != "LEFT"){
+	d = "RIGHT";
+	} else if (event.keyCode == 40 && d != "UP"){
+	d = "DOWN";
+}
+}
+
+// check collision 
+function collision(header, array){
+	for(let i = 0; i < array.length; i++){
+		if (header.x === array[i].x && header.y === array[i].y){
+			return true;
+		}
+	}
+	return false;
+}
+
+// draw everything to the canvas
+function draw() {
+ctx.drawImage(ground, 0,0);
+
+for (let i= 0; i < snake.length; i ++){
+	ctx.fillStyle = (i == 0)? "red" : "black";
+	ctx.fillRect(snake[i].x, snake[i].y,box, box);
+
+	ctx.strokeStyle = "white"
+	ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+}
+
+// draw food
+ctx.drawImage(apple, food.x, food.y);
+
+// old head position
+let snakeX = snake[0].x;
+let snakeY = snake[0].y;
+
+
+
+// which direction
+if( d==="LEFT") snakeX -= box;
+if (d === "UP") snakeY -= box;
+if( d==="RIGHT") snakeX += box;
+if( d==="DOWN") snakeY += box;
+
+// if the snake eats the food
+if(snakeX === food.x && snakeY === food.y){
+	score++;
+	// create new food (object)
+	food = {
+		x: Math.floor(Math.random() * 17 + 1) * box,
+		y: Math.floor(Math.random() * 15 + 3) * box
+	}
+	// don't remove the tail
+} else {
+	// remove the tail of the snake
+	snake.pop();
+}
+
+// new snake head
+let newHead = {
+	x: snakeX,
+	y: snakeY,
+}
+
+// snake is dead // game over
+if(snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box || collision(newHead,snake)){
+clearInterval(game);
+}
+
+
+
+snake.unshift(newHead);
+
+ctx.fillStyle = "white";
+ctx.font = "45 px";
+ctx.fillText(score,2*box,1.6*box);
+}
+
+
+// call function draw
+let game = setInterval(draw, 100)
---
+++ snake.py
@@ -0,0 +1,80 @@
+import time, random
+from sense_hat import SenseHat
+
+s= SenseHat()
+
+red = (255, 0,0)
+white = (255,255,255)
+clear =(0,0,0)
+
+snake = [[3,3]]
+direction = [1,0]
+length = 1
+
+applePos=[random.randint(0,7), random.randint(0,7)]
+
+pixels = [clear] * 64 
+
+def setDirection(d): #0 = 'up', 1 ='right', 2 = 'down', 3 ='left'
+
+  global direction
+  
+  if d == 0:
+    direction = [0, -1]
+  elif d == 1:
+    direction = [1,0]
+  elif d == 2:
+    direction = [0,1]
+  elif d == 3:
+    direction = [-1,0]
+
+  if snake[0][0] < 0:
+    snake[0][0] = 7
+  if snake[0][1] < 0:
+    snake[0][1] = 7
+  if snake[0][0] > 7:
+    snake[0][0] = 0
+  if snake[0][1] > 7:
+    snake[0][1] = 0
+    
+while True:
+  pixels = [clear] * 64
+  
+  for event in s.stick.get_events():
+    
+    if event.action == "pressed":
+      
+      if event.direction == "up":
+        setDirection(0)
+      elif event.direction == "right":
+        setDirection(1)
+      elif event.direction == "down":
+        setDirection(2)
+      elif event.direction == "left":
+        setDirection(3)
+        
+        snake.insert(0, [snake[0][0] + direction[0], snake[0][1] + direction[1]])
+  
+  if snake[0] == applePos:
+    applePos=[]
+    while applePos == []:
+      applePos = [random.randint(0,7), random.randint(0,7)]
+    if applePos in snake:
+      applePos=[]
+    length += 1
+  
+  elif snake[0] in snake[1:]: #reset length if snake runs into itself
+    length = 1
+    
+  else:
+    while len(snake) > length:
+      snake.pop()
+  for pos in snake:
+    pixels[pos[1] * 8 + pos[0]] = white
+  
+  #y * rowsize + x = color
+  pixels[applePos[1] * 8 + applePos[0]] = red
+  
+  s.set_pixels(pixels)
+  
+  time.sleep(0.15)
